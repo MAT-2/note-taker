3d6d7a2055b22236782cc63bad1c7eb339f64dc6
MAT-2 <mpcasey30@gmail.com>
2023-10-26 00:21:31 -0500

    Added note to explain app.get. Commented out app.get to use the app.use function to get all files from the public folder/directory.

--- Develop/server.js
+++ Develop/server.js
@@ -5,9 +5,14 @@ const PORT = process.env.PORT || 3001;
 //Creating new express app.
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello world!");
-});
+//Allow an HTTP GET Request to go to the root ("/") of the URL. If a request comes in, send a Hello World string.
+// app.get("/", (req, res) => {
+//   res.send("Hello world!");
+// });
+
+//Using this function allows Express to serve the static files from the public directory as shown within the files.
+app.use(express.static("public"));
+
 //PORT is being used when the app is listening to the HTTP requests.
 app.listen(PORT, () => {
   console.log(`App is currently listening at http://localhost:${PORT}`);
