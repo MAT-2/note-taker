e6138200e7c86d8dd217c5165444cf25155c7409
MAT-2 <mpcasey30@gmail.com>
2023-10-25 23:50:05 -0500

    Tested to see if express is active, using app.get to send a Hello World, followed by a app.listen that listens and receives HTTP requests.

--- Develop/server.js
+++ Develop/server.js
@@ -1,4 +1,14 @@
+//Requiring express.js library
 const express = require("express");
-
+//variable PORT to be used for HTTP request
 const PORT = process.env.PORT || 3001;
+//Creating new express app.
 const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello world!");
+});
+//PORT is being used when the app is listening to the HTTP requests.
+app.listen(PORT, () => {
+  console.log(`App is currently listening at http://localhost:${PORT}`);
+});
